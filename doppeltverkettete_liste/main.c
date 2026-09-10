ce1183f4033eebd2e31404daadbb402a5af64196
Stephen Eyring <accounts@eyring-s.de>
2026-09-10 23:28:04 +0200

    list pop front methode

--- doppeltverkettete_liste/main.c
+++ doppeltverkettete_liste/main.c
@@ -15,51 +15,49 @@ struct List {
   int size;
 };
 
-int list_push_back(struct List *list, int value){
+int list_push_back(struct List *list, int value) {
   struct Node *newNode;
   newNode = malloc(sizeof(struct Node));
 
-  if(newNode==NULL){
+  if (newNode == NULL) {
     return 1;
   }
 
-  newNode ->value = value;
-  newNode ->next = NULL;
+  newNode->value = value;
+  newNode->next = NULL;
 
-  if (list->first == NULL){
-    newNode ->prev = NULL;
+  if (list->first == NULL) {
+    newNode->prev = NULL;
 
     list->first = newNode;
     list->last = newNode;
-  }
-  else{
+  } else {
     newNode->prev = list->last;
 
-    list->first->next= newNode;
-    list->last=newNode;
+    list->first->next = newNode;
+    list->last = newNode;
   }
   list->size++;
   return 0;
 }
 
-int list_push_front(struct List *list, int value){
+int list_push_front(struct List *list, int value) {
   struct Node *newNode;
   newNode = malloc(sizeof(struct Node));
 
-  if(newNode==NULL){
+  if (newNode == NULL) {
     return 1;
   }
 
-  newNode ->value = value;
-  newNode ->next = NULL;
-  newNode ->prev = NULL;
+  newNode->value = value;
+  newNode->next = NULL;
+  newNode->prev = NULL;
 
-  if (list->first == NULL){
+  if (list->first == NULL) {
 
     list->first = newNode;
     list->last = newNode;
-  }
-  else{
+  } else {
     list->first->prev = newNode;
     newNode->next = list->first;
     list->first = newNode;
@@ -68,11 +66,32 @@ int list_push_front(struct List *list, int value){
   return 0;
 }
 
-struct Node *list_pop_front(struct List *list){
-  
-  if(list->first==NULL){
-    
+// struct Node *list_pop_front(struct List *list){
+//  doch nur den wert statt der gesamten node zurueckgeben
+//  es wird eine int fuer die value erzeugt und uebergeben, die funktion packt
+//  dort das ergebnis rein und der int rueckgabewert ist dann wirklich nur 1
+//  oder 0
+int list_pop_front(struct List *list, int *value) {
+
+  if (list->first == NULL) {
+    return 1;
   }
+
+  struct Node *oldFirst = list->first;
+  *value = oldFirst->value;
+
+  if (list->first->next == NULL) {
+    list->last = NULL;
+    list->first = NULL;
+  }
+  else{
+    list->first = list->first->next;
+    list->first->prev = NULL;
+  }
+
+  list->size--;
+  free(oldFirst);
+  return 0;
 }
 
 int main(void) {
@@ -83,26 +102,22 @@ int main(void) {
   liste.last = NULL;
   liste.size = 0;
 
-  if(list_push_back(&liste, 0) != 0){
+  if (list_push_back(&liste, 0) != 0) {
     return 1;
   }
-  if(list_push_back(&liste, 10) != 0){
+  if (list_push_back(&liste, 10) != 0) {
 
     return 1;
   }
 
   struct Node *current = liste.first;
 
-  while(current != NULL){
+  while (current != NULL) {
     printf("%d\n", current->value);
     current = current->next;
   }
 
-return 0;
+  return 0;
 
-
-
-
-  
   return 0;
 }
