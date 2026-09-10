a8eb28206c17484bf2ecbe4dbccfdb0ecf73c942
Stephen Eyring <accounts@eyring-s.de>
2026-09-10 17:18:27 +0200

    init

---
+++ test/helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  char characterName[] = "Klaus";
+  int characterAlter = 35;
+  printf("Name: %s\n", characterName);
+  printf("Alter: %d\n", characterAlter);
+  characterAlter = 23;
+  printf("Name: %s\n", characterName);
+  printf("Alter: %d\n", characterAlter);
+
+return 0;
+        
+}
